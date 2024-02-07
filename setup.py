4d1da8b9299762243f4a908bf128b7b392a0bad7
Christos Konstantinos Matzoros <christosmatzoros@gmail.com>
2024-02-07 13:24:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     packages=find_packages(),
     author="Christos Matzoros",
     author_email="christosmatzoros@gmail.com",
-    description="QIIME 2 Plugin for quality control of long read sequences,",
+    description="QIIME 2 Plugin for quality control of long read sequences.",
     url="https://github.com/bokulich-lab/q2-long-reads-qc",
     entry_points={"qiime2.plugins": ["q2-long-reads-qc=q2_long_reads_qc.plugin_setup:plugin"]},
     zip_safe=False,
