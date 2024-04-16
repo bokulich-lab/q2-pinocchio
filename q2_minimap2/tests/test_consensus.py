dba25470dc7f1b8ccf638a6eaf4c3c71bfe22fdf
Christos Matzoros <christosmatzoros@gmail.com>
2024-04-16 14:14:09 +0200

    TEST: Reform the way we import input in consensus action

--- q2_minimap2/tests/test_consensus.py
+++ q2_minimap2/tests/test_consensus.py
@@ -9,6 +9,7 @@
 
 import pandas as pd
 import pandas.testing as pdt
+from q2_types.feature_data import FeatureData, Sequence, Taxonomy
 from qiime2 import Artifact
 
 from q2_minimap2.classify_consensus import _PAF_format_df_to_series_of_lists
@@ -25,24 +26,32 @@ def series_is_subset(expected, observed):
 class TestConsensusAssignment(Minimap2TestsBase):
     def setUp(self):
         super().setUp()
-        self.taxonomy = Artifact.load(self.get_data_path("taxonomy.qza"))
-        self.reads = Artifact.load(self.get_data_path("se-dna-sequences.qza"))
+
+        self.taxonomy_tsv_path = self.get_data_path("consensus/taxonomy.tsv")
+        self.reads_path = self.get_data_path("consensus/se-dna-sequences.fasta")
         self.paf = Artifact.load(self.get_data_path("search_results.qza"))
-        self.exp = self.taxonomy.view(pd.Series)
-        self.query_reads = Artifact.load(self.get_data_path("query-seqs.qza"))
 
     def test_classify_consensus_minimap2(self):
+        taxonomy_input = Artifact.import_data(
+            FeatureData[Taxonomy], self.taxonomy_tsv_path
+        )
+        reads = Artifact.import_data(FeatureData[Sequence], self.reads_path)
+
         (paf, taxonomy) = self.plugin.pipelines["classify_consensus_minimap2"](
-            query=self.reads,
-            reference_reads=self.reads,
-            reference_taxonomy=self.taxonomy,
+            query=reads,
+            reference_reads=reads,
+            reference_taxonomy=taxonomy_input,
         )
 
-        self.assertTrue(series_is_subset(self.exp, taxonomy.view(pd.Series)))
+        expected = taxonomy_input.view(pd.Series)
+        self.assertTrue(series_is_subset(expected, taxonomy.view(pd.Series)))
 
     def test_PAF_format_df_to_series_of_lists(self):
         paf = self.paf.view(pd.DataFrame)
-        taxonomy = self.taxonomy.view(pd.Series)
+        taxonomy_input = Artifact.import_data(
+            FeatureData[Taxonomy], self.taxonomy_tsv_path
+        )
+        taxonomy = taxonomy_input.view(pd.Series)
 
         obs = _PAF_format_df_to_series_of_lists(paf, taxonomy)
         exp = pd.Series(
@@ -68,14 +77,20 @@ class TestConsensusAssignment(Minimap2TestsBase):
         self,
     ):
         paf = self.paf.view(pd.DataFrame)
-        taxonomy = self.taxonomy.view(pd.Series)
+        taxonomy_input = Artifact.import_data(
+            FeatureData[Taxonomy], self.taxonomy_tsv_path
+        )
+        taxonomy = taxonomy_input.view(pd.Series)
         paf.loc[3] = ["junk", "", "", "", "", "lost-id"] + [""] * 17
         with self.assertRaisesRegex(KeyError, "results do not match.*lost-id"):
             _PAF_format_df_to_series_of_lists(paf, taxonomy)
 
     def test_find_consensus_annotation(self):
+        taxonomy_input = Artifact.import_data(
+            FeatureData[Taxonomy], self.taxonomy_tsv_path
+        )
         (consensus,) = self.plugin.methods["_find_consensus_annotation"](
-            search_results=self.paf, reference_taxonomy=self.taxonomy
+            search_results=self.paf, reference_taxonomy=taxonomy_input
         )
         obs = consensus.view(pd.DataFrame)
 
